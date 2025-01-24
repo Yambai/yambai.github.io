3c2f64410125ca63c4247cdfd8bdf9e02a484e32
Yambai <magadiev.123789@gmail.com>
2025-01-24 23:49:33 +0500

    5

--- js/animation.js
+++ js/animation.js
@@ -1,23 +1,4 @@
-// Получаем элемент body
-const body = document.querySelector('body');
-
-// Массив цветов для фона
-const colors = ['#AEE1F8', '#FFBC8B']; // бледно-синий и бледно-оранжевый
-
-// Индекс текущего цвета
-let currentColorIndex = 0;
-
-// Функция для смены фона
-function changeBackgroundColor() {
-  // Меняем цвет фона
-  body.style.backgroundColor = colors[currentColorIndex];
-
-  // Изменяем индекс цвета для следующего перехода
-  currentColorIndex = (currentColorIndex + 1) % colors.length;
-}
-
-// Запускаем смену фона каждые 5 секунд
-setInterval(changeBackgroundColor, 5000);
-
-// Изменим фон при первой загрузке
-changeBackgroundColor();
+// script.js
+document.querySelector('.login').addEventListener('click', () => {
+  alert('РђРІС‚РѕСЂРёР·Р°С†РёСЏ РІСЂРµРјРµРЅРЅРѕ РЅРµРґРѕСЃС‚СѓРїРЅР°');
+});
